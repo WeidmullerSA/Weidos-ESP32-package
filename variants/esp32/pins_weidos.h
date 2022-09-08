30779716e1fc8bcc23899bcc4df0534518707ec9
OriolFalip <Oriol.Falip@weidmueller.com>
2022-09-08 09:47:12 +0200

    define digital output pins
    Digital output pins are connected to an I/O expander (MCP23008). From left to right, first byte to the expander's I2C address (0x21). Second byte corresponds to the expander's pin number.

--- variants/esp32/pins_weidos.h
+++ variants/esp32/pins_weidos.h
@@ -13,5 +13,16 @@ static const uint32_t pin26 = DI_5;
 static const uint32_t pin27 = DI_6;
 static const uint32_t pin28 = DI_7;
 
+
+//Digital outputs
+#define DO_0 (0x2100)
+#define DO_1 (0x2101)
+#define DO_2 (0x2102)
+#define DO_3 (0x2103)
+static const uint32_t pin55 = DO_0;
+static const uint32_t pin56 = DO_1;
+static const uint32_t pin57 = DO_2;
+static const uint32_t pin58 = DO_3;
+
 //####Weidmuller####
 #endif
