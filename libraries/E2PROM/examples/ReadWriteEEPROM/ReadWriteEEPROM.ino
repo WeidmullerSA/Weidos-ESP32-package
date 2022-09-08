d46b0d67d0229757492a8e3d670dd80d7c6a10dd
OriolFalip <Oriol.Falip@weidmueller.com>
2022-09-08 11:42:47 +0200

    Fix text in some examples

--- libraries/E2PROM/examples/ReadWriteEEPROM/ReadWriteEEPROM.ino
+++ libraries/E2PROM/examples/ReadWriteEEPROM/ReadWriteEEPROM.ino
@@ -1,3 +1,17 @@
+/**
+Copyright (C) 2021 Weidmüller SA. All rights reserved.
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 /*
   Read/Write EEPROM example
 
@@ -6,7 +20,7 @@
 
   
   First, it shows how to read/write a cell (1 byte) from the EEPROM.
-  Then two objects, an double and an integer with known values,
+  Then two objects, a double and an integer with known values,
   will be saved with E2PROM method put(); this value will be
   recovered with method get() and stored to a new variables intialized to 0.
   To check that the put/get methods were successful, those new
